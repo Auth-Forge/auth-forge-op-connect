50e6277bc77f96d3fc5df17e7385065a68750578
Steven Buglione <stevenbuglione1@gmail.com>
2024-10-18 22:13:30 -0400

    Initial Commit

---
+++ src/main/java/io/authforge/opconnect/OpConnectApplication.java
@@ -0,0 +1,13 @@
+package io.authforge.opconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpConnectApplication.class, args);
+	}
+
+}
